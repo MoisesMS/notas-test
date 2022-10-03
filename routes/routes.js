312001a5eff41a60d1c54fe0112c6c4f9ba20e8c
Moisés <moisesmorentes@gmail.com>
2022-10-03 19:18:44 +0200

    modificados los credenciales

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,13 @@
 const express = require("express")
+const path = require("path")
 const controller = require("../controllers/controller")
 
+
 const router = express.Router()
+
+router.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../public/index.html"))
+})
 
 router.get("/api/", controller.getNotes)
 router.get("/api/:id", controller.getNotesForId)
